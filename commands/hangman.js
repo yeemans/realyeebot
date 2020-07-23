dab6e93eb12a95d16ee54937266452b4af9af4ec
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-23 11:45:13 -0700

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -5,7 +5,7 @@ module.exports={
     description: 'plays hangman',
     execute() {
         var fs = require("fs");
-        var text = fs.readFileSync("./dictionary.txt");
+        var text = fs.readFileSync("./commands/dictionary.txt");
         var dictionary = text.split("\n");
         var word = dictionary[Math.floor(Math.random() * dictionary.length)];
         message.channel.send(word);
