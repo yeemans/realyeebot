5d0d58528755ff885346f44827916ffbc278de71
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-23 10:17:19 -0700

    Update pikachu.js

--- commands/pikachu.js
+++ commands/pikachu.js
@@ -2,6 +2,6 @@ module.exports={
     name: 'pikachu',
     description: 'dumb copypasta',
     execute(message, args) {
-        message.channel.send("https://imgur.com/a/ZyjanYM"); 
+        message.channel.send("Pikachu has everything you need for a character and you just need to have general game sense and mechanics to play him properly. The “this character is not very good/average in the wrong hands” is a very flawed argument as it applies to every character in the game. No person is going to be insane with Pikachu without actually learning him first but it doesn’t take long and once you do he’s really cheap. I was a former Pikachu main btw I dropped him because he felt skilless and boring and he gives you stuff for free as long as you use more than 2 of your braincells. Pika has nothing that is extremely difficult to pull of and maybe 10 minutes of labbing will allow you to pull of his “difficult” stuff. Every average player will make good/broken characters look average too. Pikachu won’t dominate this level of play because this level of play is mid level players on wifi."); 
     }
 }
