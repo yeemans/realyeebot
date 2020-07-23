18a945cc0f3cd81da72a7bb20a77adc3f3226c82
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-23 15:26:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -46,10 +46,6 @@ client.on('message', message => {
         return;
     }
 
-    if (message.content.includes('yee')) {
-        message.channel.send('Y E E E E E E E E'); 
-        return;
-    }
     
 
     if(!message.content.startsWith(prefix) || message.author.bot) return;
@@ -62,6 +58,7 @@ client.on('message', message => {
     }
     else if(command === 'yee'){
         client.commands.get('yee').execute(message, args);
+        return;
     }
     else if (command ==='wa') { 
         client.commands.get('pa').execute(message, args);
@@ -71,6 +68,10 @@ client.on('message', message => {
     }
     else if (command ==='pikachu') {
         client.commands.get('pikachu').execute(message, args);
+    }
+    if (message.content.includes('yee')) {
+        message.channel.send('Y E E E E E E E E'); 
+        return;
     }
     
 });
