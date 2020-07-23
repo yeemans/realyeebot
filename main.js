0c3045715b24618861b4d3c9a102cb02bddbc530
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-22 22:10:15 -0700

    Update main.js

--- main.js
+++ main.js
@@ -20,23 +20,21 @@ client.once('ready', () => {
 });
  
 client.on('message', message =>{
-    message.channel.send('poop');
     message.content = message.content.toLowerCase();
     if (message.content === 'hi' || message.content === 'hello' && 
     message.author.bot == false) {
         client.commands.get('shutup').execute(message, args);
     }
     
+    if (command.includes('bruh')) {
+        message.channel.send('bruh');
+    }
 
     if(!message.content.startsWith(prefix) || message.author.bot) return;
  
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     
-
-    if (command.includes('bruh')) {
-        message.channel.send('bruh');
-    }
     if(command === 'ping'){
         client.commands.get('ping').execute(message, args);
     }
