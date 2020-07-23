dd2245c016089271ba23e1a006afea77a11d54b8
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-23 12:52:54 -0700

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -6,8 +6,17 @@ module.exports={
     execute(message, args) {
         var fs = require("fs");
         var text = fs.readFileSync("./commands/dictionary.txt") + '';
+        var mistakes = 6; //6 mistakes and you lose 
         var dictionary = text.split("\n");
         var word = dictionary[Math.floor(Math.random() * dictionary.length)];
+        var underscores = '';
+        for (var a = 0; a < length(word); a++) { 
+            underscores += '_';
+        } 
+        while (mistakes > 0) {
+            mistakes -= 1; 
+            message.channel.send('' + mistakes);
+        }
         message.channel.send('' + word);
     }
 }
