f621f75fd9f73c0146f4fc55ba36b2619f053fde
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-23 11:35:56 -0700

    Add files via upload

---
+++ commands/hangman.js
@@ -0,0 +1,13 @@
+const { Message } = require("discord.js");
+
+module.exports={
+    name: 'hangman',
+    description: 'plays hangman',
+    execute() {
+        var fs = require("fs");
+        var text = fs.readFileSync("dictionary.txt");
+        var dictionary = text.split("\n");
+        var word = dictionary[Math.floor(Math.random() * dictionary.length)];
+        message.channel.send(word);
+    }
+}
