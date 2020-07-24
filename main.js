cf7fd9c378d003372627fb052140e7859dd50f11
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-24 11:33:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -129,10 +129,11 @@ client.on('message', async (message) => {
             }) 
         
         color = message.guild.roles.cache.find(r => r.name === "" + args[1]);
-        color.setPosition(0);
+        color.setPosition(1)
+        .then(updated => console.log(`Role position: ${updated.position}`))
+        .catch(console.error);color.setPosition(0);
         message.member.roles.add(color);
-        color = message.guild.roles.cache.find(r => r.name === "" + args[1]);
-        color.setPosition(0);
+
         
     }
 
