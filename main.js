0ecdc806566c2dfc610b61d6f00bf46a54b04255
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-24 14:00:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -62,12 +62,11 @@ client.on('message', async (message, member) => {
     }
     if (message.content.includes('fuck you') && !(message.author.bot)) {
         message.channel.send('fuck you too');
-        message.author.send('That was very rude.');
-        if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) {
-            message.author.send('That was very rude.');
+        if (message.guild.me.hasPermission('MANAGE_NICKNAMES')) {
+            message.member.setNickname('pphead'); 
         }
         else  {
-            message.member.setNickname('pphead');
+            message.author.send('very rude');
         }
         return;
     }
