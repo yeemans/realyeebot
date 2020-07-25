88e66dea7d410861a433c939491348cc89300ca3
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-25 15:15:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ client.on('message', async (message, member) => {
         client.commands.get('pikachu').execute(message, args);
     }
     else if (command === 'color') {
-        message.channel.send(`If this didn't work it means my role needs to be the highest in ur server.`);
+        message.channel.send(`Check your roles now. If this didn't work it means my role needs to be the highest in this server.`);
             await message.guild.roles.create({ 
                 data: {
                     name: args[1], 
