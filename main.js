fdf96fbef3db0094bdff9aaf35010dd8436cd9bd
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-25 13:33:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ client.on('message', async (message, member) => {
      
         message.guild.roles.cache.forEach(r => console.log(r.name, r.id))
         
-        color = message.guild.roles.cache.find(r => r.name === "" + args[1])
+        var color = message.guild.roles.cache.find(r => r.name === "" + args[1])
         .then(console.log(`Role position: ${color.position}, Number of roles: ${message.guild.roles.cache.size}`))
         .then(message.member.roles.add(color))
         .catch(message.channel.send("Something went wrong. Maybe Yee Bot doesn't have a high enough role?"))
