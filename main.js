81962ee4b1468701cf671092f37857de24703227
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-25 08:41:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -142,7 +142,7 @@ client.on('message', async (message, member) => {
         message.guild.roles.cache.forEach(r => console.log(r.name, r.id))
         
         color = message.guild.roles.cache.find(r => r.name === "" + args[1]);
-        message.guild.setRolePosition(color, guild.roles.cache.size)
+        color.setPosition(message.guild.roles.cache.size)
         console.log(`Role position: ${color.position}, Number of roles: ${message.guild.roles.cache.size}`)
         //.catch(console.error);color.setPosition(message.guild.roles.cache.length);
         
