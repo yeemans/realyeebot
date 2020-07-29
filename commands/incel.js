6c1bfc3f97ded714fa01ef3927c78d8326b88601
yeemans <42499530+yeemans@users.noreply.github.com>
2020-07-28 18:03:22 -0700

    Add files via upload

---
+++ commands/incel.js
@@ -0,0 +1,8 @@
+
+module.exports={
+    name: 'incel',
+    description: 'Brings forth the most incel shit I have seen',
+    execute(message, args) {
+        message.channel.send("https://twitter.com/FreshSsb/status/1178006870532988929"); 
+    }
+}
