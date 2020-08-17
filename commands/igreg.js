04b9edfac0939a47acecca5ef745f4ff6a1e50c2
yeemans <42499530+yeemans@users.noreply.github.com>
2020-08-17 12:01:52 -0700

    Add files via upload

---
+++ commands/igreg.js
@@ -0,0 +1,8 @@
+
+module.exports={
+    name: 'igreg',
+    description: 'links igreg twitter',
+    execute(message, args) {
+        message.channel.send("https://twitter.com/iGreg7?s=09"); 
+    }
+}
